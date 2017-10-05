0ac7e62265acf9dca3d0940bf0697d23e1c9a570
rubyxroot <rubyxdesign@yahoo.com>
2017-10-05 06:30:43 +0000

    Update Filipino.js

--- src/translations/Filipino.js
+++ src/translations/Filipino.js
@@ -4,7 +4,7 @@ export const FILIPINO_TRANSLATION = {
     address: 'Address',
     privateKey: 'Pribadong Susi (Private Key)',
     cancel: 'Kansela',    
-    fees: 'Fees',
+    fees: 'Bayad',
     version: 'Bersyon',
     in: 'Papasok',
     out: 'Palabas'
@@ -25,7 +25,7 @@ export const FILIPINO_TRANSLATION = {
     payTo: 'Ipadala sa',
     amountToPay: 'Halaga na Ipadala',
     balance: 'Balanse',
-    networkFee: 'Network Fee',
+    networkFee: 'Bayad sa Network',
     slowTx: 'Mabagal na Tx',
     fastTx: 'Mabilis na Tx',    
     from: 'Mula sa',
@@ -37,7 +37,7 @@ export const FILIPINO_TRANSLATION = {
     confirmSend: 'Gusto ko ipadala ang ZEN na ito.',
     invalidAddress: 'Hindi tamang `Patungong Address` transparent na addresses lang ang supportado sa ngayon.',
     invalidAmount: 'Hindi tamang `Halaga`.',
-    invalidFee: 'Hindi tamang `Fees`. Subukan 0 :)',
+    invalidFee: 'Hindi tamang `Bayad(Fees)`. Subukan 0 :)',
     zeroAmount: 'Mas mataaas sa 0 dapat ang halaga na kailangan.',
     notEnoughZEN: 'Hindi sapat na kumpirmadong ZEN sa account para gawin ang transaksyon.',
     noCameraPermissions: 'Walang pahintulot sa camera. Pwede mong pahintulutan ang camera sa iyong settings.'
