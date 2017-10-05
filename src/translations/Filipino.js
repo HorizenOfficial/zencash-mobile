d24dd554828a31811e8a691256587f967856bd54
rubyxroot <rubyxdesign@yahoo.com>
2017-10-05 06:15:18 +0000

    Filipino.js
    Filipino Translation

---
+++ src/translations/Filipino.js
@@ -0,0 +1,86 @@
+export const FILIPINO_TRANSLATION = {
+  General: {
+    loading: 'Nagsisimula...',
+    address: 'Address',
+    privateKey: 'Pribadong Susi (Private Key)',
+    cancel: 'Kansela',    
+    fees: 'Fees',
+    version: 'Bersyon',
+    in: 'Papasok',
+    out: 'Palabas'
+  },
+  MainPage: {
+    title: 'ZEN Wallet',
+    value: 'Halaga',        
+    send: 'Padala',
+    received: 'Natanggap',
+    sent: 'Napadala',    
+    noTxFound: 'Walang transaksyon na nakita.'
+  },
+  AddressInfoPage: {
+    copyToClipboard: 'Kopyahin ang Address sa Clipboard',
+  },
+  SendPage: {
+    title: 'Magpadala ng ZEN',
+    payTo: 'Ipadala sa',
+    amountToPay: 'Halaga na Ipadala',
+    balance: 'Balanse',
+    networkFee: 'Network Fee',
+    slowTx: 'Mabagal na Tx',
+    fastTx: 'Mabilis na Tx',    
+    from: 'Mula sa',
+    toAddress: 'Patungong Address',
+    amount: 'Halaga',    
+    max: 'Lahat na',    
+    send: 'Ipadala',
+    txSuccessful: 'Tagumpay na Transaksyon! Click dito para makita ang detalye ng transaksyon.',
+    confirmSend: 'Gusto ko ipadala ang ZEN na ito.',
+    invalidAddress: 'Hindi tamang `Patungong Address` transparent na addresses lang ang supportado sa ngayon.',
+    invalidAmount: 'Hindi tamang `Halaga`.',
+    invalidFee: 'Hindi tamang `Fees`. Subukan 0 :)',
+    zeroAmount: 'Mas mataaas sa 0 dapat ang halaga na kailangan.',
+    notEnoughZEN: 'Hindi sapat na kumpirmadong ZEN sa account para gawin ang transaksyon.',
+    noCameraPermissions: 'Walang pahintulot sa camera. Pwede mong pahintulutan ang camera sa iyong settings.'
+  },
+  TxDetailPage:{
+    txid: 'Transaksyong Id',
+    blockhash: 'Block Hash',
+    blockheight: 'Block Height',
+    confirmations: 'Mga Kumpirmasyon',    
+  },
+  SettingsPage: {
+    title: 'Settings',    
+    language: 'Lengguwahe',
+    currency: 'Salapi',
+    secretPhrase: 'Pakita ang Pribadong Kataga',
+    showPrivateKeys: 'Pakita ang Pribadong Susi(Private Keys)',
+    recoverExistingWallet: 'Karga muli ang dating Wallet',
+    current: 'Kasalukuyan'
+  },  
+  SecretPhrasePage: {
+    title: 'Pribadong Kataga(Secret Phrase)'
+  },
+  ShowPrivateKeyPage: {
+    title: 'Pribadong Susi(Private Keys)'
+  },
+  RecoverExistingWalletPage: {
+    title: 'Karga muli ang dating Wallet',
+    secretPhrase: 'Pribadong Kataga(Secret Phrase)',
+    textareaPlaceholder: 'Pribadong Kataga. Hindi dapat bababa sa 16 na letra o numero',
+    confirmUnderstand: 'Naintindihan ko na pag kinarga ko ang dating wallet, ang kasalukuyang wallet ay mabubura.',
+    recover: 'Mag karga muli'
+  },
+  AboutPage: {
+    title: 'Tungkol sa...',
+  },
+  PinPage: {
+    changePinTitle: 'Palitan ang PIN',
+    newPinPageTitle: 'ZEN Wallet Setup',
+    verifyPinPageTitle: 'Pagpapatunay ng ZEN Wallet PIN',
+    setupNewPin: 'Gumawa ng bagong PIN',
+    reenterPin: 'Lagay ulit ang PIN',
+    pinsNotSimilar: 'Hindi tugma ang PINs na nailagay',
+    invalidPin: 'Hindi tamang PIN',
+    enterYourPin: 'Lagay ang iyong PIN'
+  }
+}
