c33db30009d14bfd4702b1370c6f82914c291238
Aleksandar Berar <aleksandar.berar@gmail.com>
2017-10-14 13:19:45 +0200

    Add files via upload
    Added Serbian.js

---
+++ src/translations/Serbian.js
@@ -0,0 +1,86 @@
+export const SERBIAN_TRANSLATION = {
+  General: {
+    loading: 'učitavam...',
+    address: 'Adresa',
+    privateKey: 'Privatni Ključ',
+    cancel: 'Otkaži',    
+    fees: 'Provizije',
+    version: 'Verzija',
+    in: 'Ulaz',
+    out: 'Izlaz'
+  },
+  MainPage: {
+    title: 'ZEN Novčanik',
+    value: 'Vrednost',        
+    send: 'Pošalji',
+    received: 'Primljene',
+    sent: 'Poslate',    
+    noTxFound: 'Nema istorije transakcija.'
+  },
+  AddressInfoPage: {
+    copyToClipboard: 'Prekopiraj Adresu na Clipboard',
+  },
+  SendPage: {
+    title: 'Pošalji ZEN',
+    payTo: 'Plati',
+    amountToPay: 'Suma Za Plaćanje',
+    balance: 'Balans',
+    networkFee: 'Mrežna Provizija',
+    slowTx: 'Spora Tx',
+    fastTx: 'Brza Tx',    
+    from: 'Od',
+    toAddress: 'Za Adresu',
+    amount: 'Suma',    
+    max: 'Max',    
+    send: 'Pošalji',
+    txSuccessful: 'Uspešna transakcija! Kliknite ovde da vidite vašu transakciju.',
+    confirmSend: 'Želim da pošaljem ove ZEN',
+    invalidAddress: 'Nevažeća `Za Adresu` Samo transparentne adrese su trenutno podržane.',
+    invalidAmount: 'Nevažeća `Suma`.',
+    invalidFee: 'Nevažeća `Provizije`. Probajte 0 :)',
+    zeroAmount: 'Suma mora biti veća od 0.',
+    notEnoughZEN: 'Nedovoljno potvrđenih ZEN na nalogu da bi se obavila transakcija.',
+    noCameraPermissions: 'Nema dozvole za kameru. Možete dozvoliti pristup kameri u vašim podešavanjima.'
+  },
+  TxDetailPage:{
+    txid: 'Id Transakcije',
+    blockhash: 'Haš Bloka',
+    blockheight: 'Visina Bloka',
+    confirmations: 'Potvrde',    
+  },
+  SettingsPage: {
+    title: 'Podešavanja',    
+    language: 'Jezik',
+    currency: 'Valuta',
+    secretPhrase: 'Prikaži Tajnu Frazu',
+    showPrivateKeys: 'Prikaži Privatne Ključeve',
+    recoverExistingWallet: 'Oporavi Postojeći Novčanik',
+    current: 'Trenutni'
+  },  
+  SecretPhrasePage: {
+    title: 'Tajna Fraza'
+  },
+  ShowPrivateKeyPage: {
+    title: 'Privatni Ključevi'
+  },
+  RecoverExistingWalletPage: {
+    title: 'Oporavi Postojeći Novčanik',
+    secretPhrase: 'Tajna Fraza',
+    textareaPlaceholder: 'Tajna fraza. min 16 karaktera',
+    confirmUnderstand: 'Shvatam da će oporavljanje postojećeg novčanika, obrisati moj trenutni novčanik.',
+    recover: 'Oporavi'
+  },
+  AboutPage: {
+    title: 'O Programu',
+  },
+  PinPage: {
+    changePinTitle: 'Promeni PIN',
+    newPinPageTitle: 'Podešavanje ZEN Novčanika',
+    verifyPinPageTitle: 'ZEN Novčanik PIN Verifikacija',
+    setupNewPin: 'Postavite novi PIN',
+    reenterPin: 'Ponovo unesite PIN',
+    pinsNotSimilar: 'Uneti PINovi se ne slažu',
+    invalidPin: 'Nevažeći PIN',
+    enterYourPin: 'Unesite vaš PIN'
+  }
+}
