f8d8dc1871d449cc3269e4acc0df8d5b0238f644
Aleksandar Berar <aleksandar.berar@gmail.com>
2017-10-14 12:56:19 +0200

    Update index.js

--- src/translations/index.js
+++ src/translations/index.js
@@ -6,10 +6,10 @@ import {
   LANG_INDONESIAN,
   LANG_PORTUGUESE,
   LANG_RUSSIAN,
+  LANG_SERBIAN,
   LANG_SPANISH,
   LANG_SWEDISH,
-  LANG_AFRIKAANS, 
-  LANG_SERBIAN
+  LANG_AFRIKAANS
 } from '../actions/Settings';
 
 import { ENGLISH_TRANSLATION } from './English.js';
@@ -19,10 +19,10 @@ import { GERMAN_TRANSLATION } from './German.js';
 import { INDONESIAN_TRANSLATION } from './Indonesian.js';
 import { PORTUGUESE_TRANSLATION } from './Portuguese.js';
 import { RUSSIAN_TRANSLATION } from './Russian.js';
+import { SERBIAN_TRANSLATION } from './Serbian.js';
 import { SPANISH_TRANSLATION } from './Spanish.js';
 import { SWEDISH_TRANSLATION } from './Swedish.js';
 import { AFRIKAANS_TRANSLATION } from './Afrikaans.js';
-import { SERBIAN_TRANSLATION } from './Serbian.js';
 
 var translations = {};
 
@@ -34,9 +34,9 @@ translations[LANG_GERMAN] = GERMAN_TRANSLATION;
 translations[LANG_INDONESIAN] = INDONESIAN_TRANSLATION
 translations[LANG_PORTUGUESE] = PORTUGUESE_TRANSLATION;
 translations[LANG_RUSSIAN] = RUSSIAN_TRANSLATION;
+translations[LANG_SERBIAN] = SERBIAN_TRANSLATION;
 translations[LANG_SPANISH] = SPANISH_TRANSLATION;
 translations[LANG_SWEDISH] = SWEDISH_TRANSLATION;
 translations[LANG_AFRIKAANS] = AFRIKAANS_TRANSLATION;
-translations[LANG_SERBIAN] = SERBIAN_TRANSLATION;
 
 export default translations
